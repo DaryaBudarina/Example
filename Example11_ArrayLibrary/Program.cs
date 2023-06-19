9d0db26b5a77a088307f8b022368bb516d087573
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-19 12:10:28 +0600

    add example 11

---
+++ Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index +1;
+        // index +=1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // void - метод, который НЕ возвращает значения
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break; // break - оператор, который завершает алгоритм
+        }
+        index++;
+    }
+    return position; // return - метод, который возвращает значения
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
